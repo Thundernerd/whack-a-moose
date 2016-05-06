9a053aa66a729b6238dcb115d8884f024053e171
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-05-06 17:00:34 +0200

    Ola

---
+++ Whack-a-Moose/Assets/RotationWizard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RotationWizard : MonoBehaviour {
+
+    [Header( "David->Christian->Christiaan->Erik->Chi->Yero" )]
+    public PersonRotator[] Peeps;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        if ( Input.GetKeyUp( KeyCode.Space ) ) {
+            StartCoroutine( FireEmAll() );
+        }
+    }
+
+    private IEnumerator FireEmAll() {
+        for ( int i = 0; i < Peeps.Length; i++ ) {
+            Peeps[i].Rotate();
+            yield return new WaitForSeconds( 0.2f );
+        }
+
+        yield break;
+    }
+}
