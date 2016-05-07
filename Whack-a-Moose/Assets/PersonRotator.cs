ea2cb4089a5d0ec1e5c3651f4144adceba89656c
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-05-07 12:27:16 +0200

    Added keyboard icons

--- Whack-a-Moose/Assets/PersonRotator.cs
+++ Whack-a-Moose/Assets/PersonRotator.cs
@@ -1,22 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PersonRotator : MonoBehaviour {
 
     public GameObject N1;
     public GameObject N2;
+    public GameObject N3;
 
     private SpriteRenderer s1;
     private SpriteRenderer s2;
+
+    private Image img;
+    private Text txt;
 
     void Start() {
         s1 = N1.GetComponent<SpriteRenderer>();
         s2 = N2.GetComponent<SpriteRenderer>();
 
+        img = N3.GetComponent<Image>();
+        txt = N3.GetComponentInChildren<Text>();
+
         s2.enabled = false;
+        img.enabled = false;
+        txt.enabled = false;
     }
 
     void Update() {
-        
+        //if ( Input.GetKeyUp(...) ) {
+
+        //}
+    }
+
+    public void SetLetter( string value ) {
+        txt.text = value;
     }
 
     public void Rotate() {
@@ -29,6 +45,8 @@ public class PersonRotator : MonoBehaviour {
                 if ( value >= 90 && s1.enabled ) {
                     s1.enabled = false;
                     s2.enabled = true;
+                    img.enabled = true;
+                    txt.enabled = true;
                 }
             } ).setOnComplete( () => {
 
@@ -45,6 +63,8 @@ public class PersonRotator : MonoBehaviour {
                 if ( value <= 90 && s2.enabled ) {
                     s1.enabled = true;
                     s2.enabled = false;
+                    img.enabled = false;
+                    txt.enabled = false;
                 }
             } ).setOnComplete( () => {
 
