966801ad5dcb76f3f081380271b6a0042ebf832a
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-05-07 12:27:28 +0200

    Setting key randomsly

--- Whack-a-Moose/Assets/RotationWizard.cs
+++ Whack-a-Moose/Assets/RotationWizard.cs
@@ -6,6 +6,8 @@ public class RotationWizard : MonoBehaviour {
     [Header( "David->Christian->Christiaan->Erik->Chi->Yero" )]
     public PersonRotator[] Peeps;
 
+    private string[] chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+
     void Start() {
 
     }
@@ -17,6 +19,10 @@ public class RotationWizard : MonoBehaviour {
     }
 
     private IEnumerator FireEmAll() {
+        for ( int i = 0; i < Peeps.Length; i++ ) {
+            Peeps[i].SetLetter( chars[Random.Range( 0, chars.Length )].ToUpper() );
+        }
+
         for ( int i = 0; i < Peeps.Length; i++ ) {
             Peeps[i].Rotate();
             yield return new WaitForSeconds( 0.2f );
